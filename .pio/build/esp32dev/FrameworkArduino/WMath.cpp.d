8cbda57d85ac3c9487c39f7e083c9c3be30a0193
Wajih Mechlawi <wajih.mechlawi@micropolis.ae>
2024-03-21 15:57:31 +0400

    edit .gitignore file

---
+++ .pio/build/esp32dev/FrameworkArduino/WMath.cpp.d
@@ -0,0 +1,18 @@
+.pio/build/esp32dev/FrameworkArduino/WMath.cpp.o: \
+ /home/microspot/.platformio/packages/framework-arduinoespressif32/cores/esp32/WMath.cpp \
+ /home/microspot/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_system/include/esp_system.h \
+ /home/microspot/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
+ /home/microspot/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h \
+ /home/microspot/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_attr.h \
+ /home/microspot/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_bit_defs.h \
+ /home/microspot/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_idf_version.h \
+ /home/microspot/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_mac.h \
+ /home/microspot/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_chip_info.h \
+ /home/microspot/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_random.h \
+ /home/microspot/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-log.h \
+ /home/microspot/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_timer/include/esp_timer.h \
+ /home/microspot/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/log/include/esp_log.h \
+ /home/microspot/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_rom/include/esp_rom_sys.h \
+ /home/microspot/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/soc/esp32/include/soc/reset_reasons.h \
+ /home/microspot/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_rom/include/esp32/rom/ets_sys.h \
+ /home/microspot/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/log/include/esp_log_internal.h
---
+++ .pio/build/esp32dev/lib69a/Rosserial Arduino Library/time.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/lib69a/Rosserial\ Arduino\ Library/time.cpp.o: \
+ .pio/libdeps/esp32dev/Rosserial\ Arduino\ Library/src/time.cpp \
+ .pio/libdeps/esp32dev/Rosserial\ Arduino\ Library/src/ros/time.h \
+ .pio/libdeps/esp32dev/Rosserial\ Arduino\ Library/src/ros/duration.h
