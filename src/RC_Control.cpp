c25ef9d38d8f700f1b5a8e78b7741d05af4cefbe
= <=>
2024-03-30 14:46:37 +0400

    update RC_Cmd and ROS_Cmd commands

--- src/RC_Control.cpp
+++ src/RC_Control.cpp
@@ -6,47 +6,45 @@ void bluetooth_init()
 {
     SerialBT.begin("ESP32s");
 }
+char rc_input = ' ';
 
-void 
-
-RC_Control(float &linear_velocity, float &angular_velocity)
+void RC_Control(float &linear_velocity, float &angular_velocity)
 {
     is_bluetooth_connected = SerialBT.connected();
     if (!is_bluetooth_connected)
     {
+        rc_input = 's';
         return;
     }
-
-    char rc_input = 'e';
 
     if (SerialBT.available())
     {
         rc_input = SerialBT.read();
-        switch (rc_input)
-        {
-        case 's':
-            linear_velocity = 0.0;
-            angular_velocity = 0.0;
-            break;
+    }
+    switch (rc_input)
+    {
+    case 's':
+        linear_velocity = 0.0;
+        angular_velocity = 0.0;
+        break;
 
-        case 'f':
-            // linear_velocity = linear_velocity == -cmd_velocity_limit ? 0.0 : linear_velocity == 0.0 ? cmd_velocity_limit: velocity + 0.01;
-            linear_velocity += 0.05;
-            break;
+    case 'f':
+        // linear_velocity = linear_velocity == -cmd_velocity_limit ? 0.0 : linear_velocity == 0.0 ? cmd_velocity_limit: velocity + 0.01;
+        linear_velocity += 0.05;
+        break;
 
-        case 'b':
-            // linear_velocity = linear_velocity == cmd_velocity_limit ? 0.0 : linear_velocity == 0.0 ? -cmd_velocity_limit: velocity - 0.01;
-            linear_velocity -= 0.05;
-            break;
+    case 'b':
+        // linear_velocity = linear_velocity == cmd_velocity_limit ? 0.0 : linear_velocity == 0.0 ? -cmd_velocity_limit: velocity - 0.01;
+        linear_velocity -= 0.05;
+        break;
 
-        case 'l':
-            angular_velocity += 0.07;
-            break;
+    case 'l':
+        angular_velocity += 0.07;
+        break;
 
-        case 'r':
-            angular_velocity -= 0.07;
-            break;
-        }
+    case 'r':
+        angular_velocity -= 0.07;
+        break;
     }
     // return rc_input;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -14,33 +14,51 @@ void setup()
   Rosserial_init();
   pinMode(2, OUTPUT);
 }
+std::pair<float, float> RC_Cmd(0, 0);
+std::pair<float, float> Ros_Cmd;
 
 void loop()
 {
+  RC_Control(RC_Cmd.first, RC_Cmd.second); // zeros if not connected
 
   if (!is_ros_connected && !is_bluetooth_connected)
   {
     linear = angular = 0;
   }
-
-  else // ros is connected
+  else
   {
-    // drive motors from laptop
-    linear = get_linear_velocity();
-    angular = get_angular_velocity();
 
-    if (millis() - previous_millis > time_interval)
+    if (!is_ros_connected) // RC but no ROS
     {
-      publish_odom(getOdom());
-      previous_millis = millis();
+      linear = RC_Cmd.first;
+      angular = RC_Cmd.second;
+    }
+    else // ROS connected
+    {
+      Ros_Cmd.first = get_linear_velocity();
+      Ros_Cmd.second = get_angular_velocity();
+
+      if (Ros_Cmd.first == 0 && Ros_Cmd.second == 0)
+      { // in case bt is not connected RC_Cmd is 0 and in case it is connected it will take the last value of RC_Cmd which it should be for the first time 0
+        linear = RC_Cmd.first;
+        angular = RC_Cmd.second;
+      }
+      else
+      {
+        linear = Ros_Cmd.first;
+        angular = Ros_Cmd.second;
+      }
+
+      if (millis() - previous_millis > time_interval)
+      {
+        publish_odom(getOdom());
+        previous_millis = millis();
+      }
     }
   }
-
-  RC_Control(linear, angular);
 
   driveMotors(linear, angular);
 
   // ros_spin
   ROS_Update();
 }
-
